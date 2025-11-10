9bdecb39d152af0c97a283b72ef78d8b0d3b3816
jamesk1799 <jamesk1799@gmail.com>
2025-11-10 15:04:36 -0800

    Create LibraryStorage.cpp

---
+++ LibraryStorage.cpp
@@ -0,0 +1,27 @@
+#include "LibraryStorage.h"
+
+
+LibraryStorage::LibraryStorage(int rows){
+    //vector<int>(columns) creates a shelf with "column" # of compartments
+    storage.resize(rows);
+}
+
+vector<Item*>& LibraryStorage::operator[](int row){
+    //Access specific shelf
+    return storage[row];
+}
+
+void LibraryStorage::printStorage() const{
+
+}
+
+void LibraryStorage::addItem(int rowNum, Item* item){
+    
+}
+
+void LibraryStorage::swapItems(int row1, int column1, int row2, int column2){
+    //Classic swap using temp variable
+    Item* temp = storage[row1][column1];
+    storage[row1][column1] = storage[row2][column2];
+    storage[row2][column2] = temp;
+}

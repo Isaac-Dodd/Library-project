08fbdb2578c951550a215dfc428014487070d48f
Isaac Dodd <143364036+Isaac-Dodd@users.noreply.github.com>
2025-11-16 15:03:07 -0800

    Update Magazine.cpp

--- Magazine.cpp
+++ Magazine.cpp
@@ -4,11 +4,30 @@ Magazine::Magazine() : Item()
 {
 	title = "1";
 	edition = 1;
-
 }
-
+Magazine::Magazine(string n, string d, int i, string t, int e) : Item(n, d, i)
+{
+	edition = e;
+	author = a;
+}
+void setTitle(string t)
+{
+	title = t;
+}
+void setEdition(int e)
+{
+	edition = e;
+}
+string getTitle()const
+{
+	return(title);
+}
+int    getEdition()const
+{
+	return(edition);
+}
 void Magazine::getItemSpecifics() const
 {
-	cout << setw(12) << "Inherited"
-		<< "Inherited";
+	cout << setw(12) << "Title: " << title << endl
+		 << setw(12) << "Edition: " << edition << endl;
 }

ed2d67d250ece7f536044a39b292638c120802b2
Isaac Dodd <143364036+Isaac-Dodd@users.noreply.github.com>
2025-11-17 17:46:32 -0800

    Update Shelf.h

--- Shelf.h
+++ Shelf.h
@@ -1,1 +1,21 @@
+#pragma once
+#ifndef "SHELF_H"
+#define "SHELF_H"
+
+#include "Item.h"
+#include "Magazine.h"
+#include "Book.h"
+#include "Movie.h"
+  
+class Shelf
+{
+public:
+  Shelf()
+  Item* & operator[](int index); // set up compartment data type
+private:
+  vector<item*>
+};
+
+#endif
+
 

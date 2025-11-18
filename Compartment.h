29c7e7f050314fe23a72f680ab7fdcc481c19bb4
Isaac Dodd <143364036+Isaac-Dodd@users.noreply.github.com>
2025-11-17 17:52:37 -0800

    Update Compartment.h

--- Compartment.h
+++ Compartment.h
@@ -1,1 +1,23 @@
+#pragma once
+#ifndef "COMPARTMENT_H"
+#define "COMPARTMENT_H"
 
+#include "Item.h"
+#include "Magazine.h"
+#include "Book.h"
+#include "Movie.h"
+
+class Compartment
+{
+public:
+Compartment();
+Compartment(item*);
+
+friend ostream& operator <<(ostream& out, const Item& item);
+
+private:
+
+};
+
+  
+#endif

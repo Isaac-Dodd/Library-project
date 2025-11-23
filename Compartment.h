9c5fc71b06d3f2f4dd07801ff02042d5e69f2c12
coupmaster09 <80240040+coupmaster09@users.noreply.github.com>
2025-11-22 17:32:20 -0800

    Update Compartment.h

--- Compartment.h
+++ Compartment.h
@@ -1,25 +1,22 @@
 #pragma once
-#ifndef "COMPARTMENT_H"
-#define "COMPARTMENT_H"
 
 #include "Item.h"
-#include "Magazine.h"
 #include "Book.h"
 #include "Movie.h"
+#include "Magazine.h"
 
 class Compartment
 {
 public:
-Compartment();
-Compartment(item*);
+	Compartment();
+	Compartment(Item*);
+	void setCurrentItem(Item*);
+	Item* getCurrentItem();
 
-friend ostream& operator <<(ostream& out, const Item& item);
+	friend ostream& operator <<(ostream& out, const Item& item);
 
-Compartment& operator[](int index);
 
 private:
-
+	Item* currentItem;
 };
 
-  
-#endif

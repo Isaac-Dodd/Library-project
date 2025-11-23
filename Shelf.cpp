ff01f1734552b42c188e537625c6066a9cb80dfa
coupmaster09 <80240040+coupmaster09@users.noreply.github.com>
2025-11-22 17:32:52 -0800

    Update Shelf.cpp

--- Shelf.cpp
+++ Shelf.cpp
@@ -1,1 +1,11 @@
+#include "Shelf.h"
 
+Shelf::Shelf()
+{
+	// already defaults to null from each compartment default constructing to null
+}
+
+Compartment& Shelf::operator[](int index)
+{
+	return (currentShelf.at(index));
+}
